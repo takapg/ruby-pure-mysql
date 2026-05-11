c6149abf8cab21a4fc119e5650ad320ba5593b62
takapg <75102730+takapg@users.noreply.github.com>
2026-05-11 10:45:46 +0900

    Configure RSpec to start RubyPureMysql server
    Add RubyPureMysql server startup before RSpec suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,20 @@
 
 require 'bundler/setup'
 require 'mysql2'
+require 'ruby_pure_mysql'
 
 RSpec.configure do |config|
   config.color = true
   config.formatter = :documentation
+
+  # テスト開始前に 3307 ポートで自作サーバーを起動
+  config.before(:suite) do
+    Thread.new do
+      RubyPureMysql.start(port: 3307)
+    rescue Errno::EADDRINUSE
+      # 既に起動している場合は無視
+    end
+    # サーバーが立ち上がるのを少し待つ
+    sleep 0.5
+  end
 end
