4dd0345466984fa7063b4a35e95b08e5bb1d0378
takapg <75102730+takapg@users.noreply.github.com>
2026-05-13 08:19:59 +0900

    Add EofPacket class to handle EOF packets
    Implement EOF packet class for MySQL protocol.

---
+++ lib/ruby_pure_mysql/protocol/eof_packet.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module RubyPureMysql
+  module Protocol
+    # EOF パケット (EOF_Packet)
+    # サーバーからクライアントへ、一連のデータの終わりを告げるために送られます。
+    # https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_basic_eof_packet.html
+    class EofPacket < BasePacket
+      # @param status_flags [Integer] サーバーの現在の状態
+      # @param warnings [Integer] 警告の数
+      def initialize(status_flags: SERVER_STATUS_AUTOCOMMIT, warnings: 0)
+        super()
+        @status_flags = status_flags
+        @warnings = warnings
+      end
+
+      # EOF パケットのペイロードを生成します。
+      # 構造: Header(0xFE) + Warnings(2bytes) + StatusFlags(2bytes)
+      def payload
+        [
+          [0xFE].pack('C'),           # Header (常に 0xFE)
+          [@warnings].pack('v'),      # Warnings (2 bytes)
+          [@status_flags].pack('v')   # Status Flags (2 bytes)
+        ].join
+      end
+    end
+  end
+end
