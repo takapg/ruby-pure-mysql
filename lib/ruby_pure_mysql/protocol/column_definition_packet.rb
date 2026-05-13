30a77b626d84182fc3508a0694009240e19c6238
takapg <75102730+takapg@users.noreply.github.com>
2026-05-13 10:34:29 +0900

    Implement ColumnDefinitionPacket class (#22)
    * Implement ColumnDefinitionPacket class
    
    Defines the ColumnDefinitionPacket class for handling column metadata in MySQL responses.
    
    * Refactor definition_fields method for clarity
    
    * Refactor ColumnDefinitionPacket methods
    
    Refactor payload and definition fields methods for clarity.
    
    * Refactor require statements in ruby_pure_mysql.rb
    
    Removed duplicate require for 'eof_packet' and added 'column_definition_packet' and 'eof_packet' requires.
    
    * Refactor write_select_one_response to use ColumnDefinitionPacket
    
    Replaced col_def_payload method with direct column packet creation.
    
    * Add type parameter to ColumnDefinitionPacket initialization
    
    * Fix column_type parameter in write_select_one_response
    
    * Validate column name in ColumnDefinitionPacket
    
    Add validation to ensure column name is not nil or empty
    
    * Add MYSQL_TYPE_LONG constant for field types
    
    * Update column type to use MYSQL_TYPE_LONG

---
+++ lib/ruby_pure_mysql/protocol/column_definition_packet.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module RubyPureMysql
+  module Protocol
+    # Column Definition パケット (ColumnDefinitionPacket)
+    # クエリ結果のメタデータをクライアントに伝えます。
+    # https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_query_response_text_resultset_column_definition.html
+    class ColumnDefinitionPacket < BasePacket
+      # @param name [String] カラム名
+      def initialize(name:, column_type: 0xFD, character_set: CHARSET_UTF8MB4, column_length: 255)
+        super()
+        raise ArgumentError, 'name cannot be nil or empty' if name.nil? || name.empty?
+
+        @name = name
+        @column_type = column_type
+        @character_set = character_set
+        @column_length = column_length
+      end
+
+      # Column Definition パケットのペイロードを生成します。
+      def payload
+        [
+          string_metadata,
+          [0x0C].pack('C'), # length of fixed-length fields
+          [@character_set, @column_length].pack('vV'), # character_set (2b), column_length (4b)
+          [@column_type, 0x0000, 0x00, 0x0000].pack('CvCv') # type (1b), flags (2b), decimals (1b), filler (2b)
+        ].join
+      end
+
+      private
+
+      # 文字列ベースのメタデータをまとめます。
+      # 構造: catalog ("def"), schema, table, org_table, name, org_name
+      def string_metadata
+        ['def', '', '', '', @name, @name].map { |s| pack_lenc_string(s) }.join
+      end
+    end
+  end
+end
