9776461b81dcfdede19aa3769a826b94870ca985
takapg <75102730+takapg@users.noreply.github.com>
2026-05-13 09:41:09 +0900

    Refactor ColumnDefinitionPacket methods
    Refactor payload and definition fields methods for clarity.

--- lib/ruby_pure_mysql/protocol/column_definition_packet.rb
+++ lib/ruby_pure_mysql/protocol/column_definition_packet.rb
@@ -7,9 +7,6 @@ module RubyPureMysql
     # https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_query_response_text_resultset_column_definition.html
     class ColumnDefinitionPacket < BasePacket
       # @param name [String] カラム名
-      # @param column_type [Integer] カラムのデータ型（MYSQL_TYPE_VAR_STRING など）
-      # @param character_set [Integer] 文字セット ID
-      # @param column_length [Integer] カラムの最大表示幅
       def initialize(name:, column_type: 0xFD, character_set: CHARSET_UTF8MB4, column_length: 255)
         super()
         @name = name
@@ -20,25 +17,20 @@ module RubyPureMysql
 
       # Column Definition パケットのペイロードを生成します。
       def payload
-        definition_fields.join
+        [
+          string_metadata,
+          [0x0C].pack('C'), # length of fixed-length fields
+          [@character_set, @column_length].pack('vV'), # character_set (2b), column_length (4b)
+          [@column_type, 0x0000, 0x00, 0x0000].pack('CvCv') # type (1b), flags (2b), decimals (1b), filler (2b)
+        ].join
       end
 
       private
 
-      # パケットの各フィールドを定義します。
-      # 構造: catalog, schema, table, org_table, name, org_name, length, set, width, type, flags, decimals, filler
-      def definition_fields
-        [
-          pack_lenc_string('def'), pack_lenc_string(''),    # catalog (常に "def"), schema
-          pack_lenc_string(''), pack_lenc_string(''),       # table, org_table
-          pack_lenc_string(@name), pack_lenc_string(@name), # name, org_name
-          pack_lenc_int(0x0C),                              # length of fixed-length fields (常に 0x0C)
-          [@character_set].pack('v'),                       # character_set (2 bytes)
-          [@column_length].pack('V'),                       # column_length (4 bytes)
-          [@column_type].pack('C'),                         # column_type (1 byte)
-          [0x0000].pack('v'), [0x00].pack('C'),             # flags (2 bytes), decimals (1 byte)
-          [0x0000].pack('v')                                # filler (2 bytes)
-        ]
+      # 文字列ベースのメタデータをまとめます。
+      # 構造: catalog ("def"), schema, table, org_table, name, org_name
+      def string_metadata
+        ['def', '', '', '', @name, @name].map { |s| pack_lenc_string(s) }.join
       end
     end
   end
